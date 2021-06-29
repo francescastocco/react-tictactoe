72efd258770514e9301d86b03146dab5e255e43a
Francesca Stocco <francescaj.stocco@gmail.com>
2021-06-29 21:30:52 +0100

    add History buttons

--- tictactoe/src/Game.jsx
+++ tictactoe/src/Game.jsx
@@ -3,17 +3,78 @@ import './App.css';
 import { Board } from './components/Board';
 
 export function Game() {
-    const [history, setHistory] = useState([]);
+    const [history, setHistory] = useState([{ squares: [null, null, null, null, null, null, null, null, null] }]);
     const [xIsNext, setXIsNext] = useState(true);
-    
+    const [stepNumber, setStepNumber] = useState(0);
+    const current = history[stepNumber];
+    let status;
+
+    const winner = calculateWinner(current.squares);
+    if (winner) {
+        status = `Winner: ${winner}`;
+    } else {
+        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+
+    function jumpTo(step) {
+        setStepNumber(step);
+        setXIsNext(step % 2 === 0);
+    }
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            `Go to move #${move}` :
+            `Go to game start`;
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    function handleClick(i) {
+        const historyNew = history.slice(0, stepNumber + 1);
+        const squares = current.squares.slice();
+        // check line 58 to see if i can remove the usestate for squares
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory(historyNew.concat([{ squares: squares }]));
+        setStepNumber(history.length);
+        setXIsNext(!xIsNext);
+    }
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board
+                    squares={current.squares}
+                    onClick={(i) => handleClick(i)} />
             </div>
             <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
+                <div className="status">{status}</div>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
--- tictactoe/src/components/Board.jsx
+++ tictactoe/src/components/Board.jsx
@@ -1,55 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Square } from './Square';
 
-export function Board() {
-    const [squares, setSquares] = useState([null, null, null, null, null, null, null, null, null])
-    const [xIsNext, setXIsNext] = useState(true);
-    const winner = calculateWinner(squares);
-    let status;
-   
-    if (winner) {
-        status = `Winner: ${winner}`;
-    } else {
-        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-    }
-
-    function calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    }
-
-    function handleClick(i) {
-        const squaresCopy = squares;
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squaresCopy[i] = xIsNext ? 'X' : 'O';
-        setSquares(squaresCopy);
-        setXIsNext(!xIsNext);
-    }
-
+export function Board(props) {
+    
     function renderSquare(i) {
-        return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+        return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
     }
 
     return (
         <div>
-            <div className="status">{status}</div>
             <div className="board-row">
                 {renderSquare(0)}
                 {renderSquare(1)}
